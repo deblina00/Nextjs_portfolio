06a6acc5c68e13c8b13c8962b896bc8477abc1e0
deblina00 <roydeblinasec@gmail.com>
2025-03-22 17:12:16 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
+console.log("Sanity Dataset:", process.env.NEXT_PUBLIC_SANITY_DATASET);
+console.log("Sanity Project ID:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+
 const nextConfig: NextConfig = {
   devIndicators: false,
   /* config options here */
